bc10744240ece40974f3d151cc77a702390b2d48
pierre.burton91 <pierre.burton91@gmail.com>
2021-02-24 15:00:43 +0100

    update 2021

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,24 +1,6 @@
 var nav = document.querySelector('nav'),
     navLinks = document.querySelectorAll('.nav__menu ul li'),
     sections = document.querySelectorAll('header, section'),
-    servicesBg = document.querySelector('.services-background'),
-    services = document.querySelectorAll('.service-item'),
-    previous = document.querySelector('.previous'),
-    next = document.querySelector('.next'),
-    i = 5;
-    serviceDesc = [
-        {description: 'Soyez vu sur internet avec un site vitrine.<br>Mettez en avant votre marque, vos produits ou vos services !', subject: 'un site vitrine'},
-        {description: 'Vendez vos produits sur internet et augmentez votre visibilité et vos revenus grâce à un site e-commerce.<br>Ce dernier sera fait sur mesure pour votre business.', subject: 'un site e-commerce'},
-        {description: 'Un blog est le meilleur de moyen de s\'exprimer sur internet.<br>Partagez vos idées, vos découvertes ou tout ce qui vous chante dans un univers à votre image.', subject: 'un blog'},
-        {description: 'Donnez vie à votre projet grâce aux technologies qu\'offre le web.<br>C\'est un moyen rapide et facile pour atteindre votre cible.' , subject: 'une application web'},
-        {description: 'Profitez de la puissance et des fonctionnalités qu\'offrent les périphériques en créant votre application native<br>pour ordinateurs ou dispositifs mobiles.', subject: 'une application native'},
-        {description: 'Soyez vu sur internet avec un site vitrine.<br>Mettez en avant votre marque, vos produits ou vos services !', subject: 'un site vitrine'},
-        {description: 'Vendez vos produits sur internet et augmentez votre visibilité et vos revenus grâce à un site e-commerce.<br>Ce dernier sera fait sur mesure pour votre business.', subject: 'un site e-commerce'},
-        {description: 'Un blog est le meilleur de moyen de s\'exprimer sur internet.<br>Partagez vos idées, vos découvertes ou tout ce qui vous chante dans un univers à votre image.', subject: 'un blog'},
-        {description: 'Donnez vie à votre projet grâce aux technologies qu\'offre le web.<br>C\'est un moyen rapide et facile pour atteindre votre cible.' , subject: 'une application web'},
-        {description: 'Profitez de la puissance et des fonctionnalités qu\'offrent les périphériques en créant votre application native<br>pour ordinateurs ou dispositifs mobiles.', subject: 'une application native'}
-    ],
-    serviceDescContainer = document.querySelectorAll('.service-description'),
     textarea = document.querySelector('textarea');
 
 document.addEventListener('scroll', function() {
@@ -48,75 +30,6 @@ document.addEventListener('scroll', function() {
     }
 });
 
-services.forEach(function(elem, index) {
-    var service = serviceDesc[index],
-        name = elem.querySelector('h4').innerHTML,
-        description = service.description,
-        imageSrc = elem.querySelector('img').getAttribute('src'),
-        message = "Bonjour Pierre,__Je serais intéressé(e) par la création d\'"+service.subject+".__Merci de me recontacter !_Cordialement";
-    
-        elem.addEventListener('mouseover', function() {
-            services.forEach(function(e, i) {
-                e.classList.remove('active');
-            })
-            elem.classList.add('active');
-            servicesBg.style.backgroundImage = "url("+imageSrc+")";
-            servicesBg.style.animationName = "slide";
-            servicesBg.style.animationDuration = '0.3s';
-            servicesBg.style.animationFillMode = 'forwards';
-            servicesBg.style.animationTimingFunction = 'ease';
-
-            updateServiceDesc(description, message, 0);
-        });
-        elem.addEventListener('mouseleave', function() {
-            servicesBg.style.animationName = '';
-            servicesBg.style.animationDuration = '';
-            servicesBg.style.animationFillMode = '';
-            servicesBg.style.animationTimingFunction = '';
-        });
-});
-
-next.addEventListener('click', function() {
-    services[i].classList.remove('active');
-    i++;
-    services[i].classList.add('active');
-
-    var service = serviceDesc[i],
-        description = service.description,
-        message = "Bonjour Pierre,__Je serais intéressé(e) par la création d\'"+service.subject+".__Merci de me recontacter !_Cordialement";
-    updateServiceDesc(description, message, 1);
-    if (i > 5) {
-        previous.style.display = "block";
-    }
-    if (i == 9) {
-        next.style.display = "none";
-    }
-});
-previous.addEventListener('click', function() {
-    services[i].classList.remove('active');
-    i--;
-    services[i].classList.add('active');
-
-    var service = serviceDesc[i],
-        description = service.description,
-        message = "Bonjour Pierre,__Je serais intéressé(e) par la création d\'"+service.subject+".__Merci de me recontacter !_Cordialement";
-    updateServiceDesc(description, message, 1);
-    if (i > 5) {
-        next.style.display = "block";
-    }
-    if (i == 5) {
-        previous.style.display = "none";
-    }
-});
-
-function updateServiceDesc(desc, message, isMobile) {
-    var descToUpdate = serviceDescContainer[isMobile].querySelector('p'),
-        buttonToUpdate = serviceDescContainer[isMobile].querySelector('button');
-    
-    descToUpdate.innerHTML = desc;
-    buttonToUpdate.setAttribute('message', ''+message+'');
-}
-
 $(document).ready(function(){
 /* Animated scroll */
     $('.scrollTo').click(function() {
@@ -126,13 +39,6 @@ $(document).ready(function(){
             $('html, body').animate( { scrollTop: 0 }, speed );
         }
         else {
-            if ($(this).attr('message') != undefined) {
-                var text = $(this).attr('message').replace(/_/g, '\n');
-                $('textarea').val(text);
-            }
-            else {
-                $('textarea').val('');
-            }
             $('html, body').animate( { scrollTop: $(page).offset().top }, speed );
         }
         $('.nav__hamburger, .nav__menu').removeClass('active');
